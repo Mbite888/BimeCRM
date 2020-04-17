fe172b999c69e26b79d3d80c0f6bf3b6b2b526d5
mbite888 <mbite888@gmail.com>
2020-04-17 21:13:57 +0430

    Creat main form manage page and page view manage  menu and items

--- BimeCRM/BimeCRM/MainPage.Designer.cs
+++ BimeCRM/BimeCRM/MainPage.Designer.cs
@@ -28,19 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.radMenu1 = new Telerik.WinControls.UI.RadMenu();
+            this.radMenuItem1 = new Telerik.WinControls.UI.RadMenuItem();
+            this.menuBimeAll = new Telerik.WinControls.UI.RadMenuItem();
+            this.menuBimeSales = new Telerik.WinControls.UI.RadMenuItem();
+            this.menuBimeBadane = new Telerik.WinControls.UI.RadMenuItem();
+            this.menuBimeOthers = new Telerik.WinControls.UI.RadMenuItem();
+            this.menuPersons = new Telerik.WinControls.UI.RadMenuItem();
+            this.menuInfo = new Telerik.WinControls.UI.RadMenuItem();
+            this.radMenuItem9 = new Telerik.WinControls.UI.RadMenuItem();
+            this.radMenuItem10 = new Telerik.WinControls.UI.RadMenuItem();
+            this.menuDatacenter = new Telerik.WinControls.UI.RadMenuItem();
+            this.visualStudio2012LightTheme1 = new Telerik.WinControls.Themes.VisualStudio2012LightTheme();
+            this.mainPageView = new Telerik.WinControls.UI.RadPageView();
+            ((System.ComponentModel.ISupportInitialize)(this.radMenu1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPageView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // radMenu1
+            // 
+            this.radMenu1.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.radMenuItem1,
+            this.menuPersons,
+            this.menuInfo,
+            this.menuDatacenter});
+            this.radMenu1.Location = new System.Drawing.Point(0, 0);
+            this.radMenu1.Name = "radMenu1";
+            this.radMenu1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.radMenu1.Size = new System.Drawing.Size(1120, 35);
+            this.radMenu1.TabIndex = 0;
+            this.radMenu1.ThemeName = "VisualStudio2012Light";
+            // 
+            // radMenuItem1
+            // 
+            this.radMenuItem1.CustomFontStyle = System.Drawing.FontStyle.Regular;
+            this.radMenuItem1.Font = new System.Drawing.Font("B Titr", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.radMenuItem1.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.menuBimeAll,
+            this.menuBimeSales,
+            this.menuBimeBadane,
+            this.menuBimeOthers});
+            this.radMenuItem1.Name = "radMenuItem1";
+            this.radMenuItem1.Padding = new System.Windows.Forms.Padding(12, 2, 12, 2);
+            this.radMenuItem1.PopupDirection = Telerik.WinControls.UI.RadDirection.Right;
+            this.radMenuItem1.RightToLeft = true;
+            this.radMenuItem1.ShowArrow = true;
+            this.radMenuItem1.Text = "بیمه ها";
+            // 
+            // menuBimeAll
+            // 
+            this.menuBimeAll.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Bold);
+            this.menuBimeAll.Name = "menuBimeAll";
+            this.menuBimeAll.Text = "همه";
+            this.menuBimeAll.Click += new System.EventHandler(this.menuBimeAll_Click);
+            // 
+            // menuBimeSales
+            // 
+            this.menuBimeSales.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Bold);
+            this.menuBimeSales.Name = "menuBimeSales";
+            this.menuBimeSales.Text = "ثالث";
+            this.menuBimeSales.Click += new System.EventHandler(this.menuBimeSales_Click);
+            // 
+            // menuBimeBadane
+            // 
+            this.menuBimeBadane.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Bold);
+            this.menuBimeBadane.Name = "menuBimeBadane";
+            this.menuBimeBadane.Text = "بدنه";
+            this.menuBimeBadane.Click += new System.EventHandler(this.menuBimeBadane_Click);
+            // 
+            // menuBimeOthers
+            // 
+            this.menuBimeOthers.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Bold);
+            this.menuBimeOthers.Name = "menuBimeOthers";
+            this.menuBimeOthers.Text = "سایر";
+            this.menuBimeOthers.Click += new System.EventHandler(this.menuBimeOthers_Click);
+            // 
+            // menuPersons
+            // 
+            this.menuPersons.Font = new System.Drawing.Font("B Titr", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.menuPersons.Name = "menuPersons";
+            this.menuPersons.Padding = new System.Windows.Forms.Padding(12, 2, 12, 2);
+            this.menuPersons.PopupDirection = Telerik.WinControls.UI.RadDirection.Right;
+            this.menuPersons.RightToLeft = true;
+            this.menuPersons.Text = "بیمه گذاران";
+            this.menuPersons.Click += new System.EventHandler(this.menuPersons_Click);
+            // 
+            // menuInfo
+            // 
+            this.menuInfo.Font = new System.Drawing.Font("B Titr", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.menuInfo.Items.AddRange(new Telerik.WinControls.RadItem[] {
+            this.radMenuItem9,
+            this.radMenuItem10});
+            this.menuInfo.Name = "menuInfo";
+            this.menuInfo.Padding = new System.Windows.Forms.Padding(12, 2, 12, 2);
+            this.menuInfo.PopupDirection = Telerik.WinControls.UI.RadDirection.Right;
+            this.menuInfo.RightToLeft = true;
+            this.menuInfo.ShowArrow = true;
+            this.menuInfo.Text = "اطلاعات";
+            // 
+            // radMenuItem9
+            // 
+            this.radMenuItem9.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.radMenuItem9.Name = "radMenuItem9";
+            this.radMenuItem9.Text = "کامنت بیمه";
+            // 
+            // radMenuItem10
+            // 
+            this.radMenuItem10.Font = new System.Drawing.Font("B Nazanin", 12F, System.Drawing.FontStyle.Bold);
+            this.radMenuItem10.Name = "radMenuItem10";
+            this.radMenuItem10.Text = "کامنت اشخاص";
+            // 
+            // menuDatacenter
+            // 
+            this.menuDatacenter.Font = new System.Drawing.Font("B Titr", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.menuDatacenter.Name = "menuDatacenter";
+            this.menuDatacenter.Padding = new System.Windows.Forms.Padding(12, 2, 12, 2);
+            this.menuDatacenter.PopupDirection = Telerik.WinControls.UI.RadDirection.Right;
+            this.menuDatacenter.RightToLeft = true;
+            this.menuDatacenter.Text = "دیتا سنتر";
+            this.menuDatacenter.Click += new System.EventHandler(this.menuDatacenter_Click);
+            // 
+            // mainPageView
+            // 
+            this.mainPageView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPageView.Location = new System.Drawing.Point(0, 35);
+            this.mainPageView.Name = "mainPageView";
+            this.mainPageView.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.mainPageView.Size = new System.Drawing.Size(1120, 663);
+            this.mainPageView.TabIndex = 1;
+            this.mainPageView.ThemeName = "VisualStudio2012Light";
+            this.mainPageView.PageRemoved += new System.EventHandler<Telerik.WinControls.UI.RadPageViewEventArgs>(this.mainPageView_PageRemoved);
+            ((Telerik.WinControls.UI.RadPageViewStripElement)(this.mainPageView.GetChildAt(0))).StripButtons = Telerik.WinControls.UI.StripViewButtons.LeftScroll;
+            ((Telerik.WinControls.UI.RadPageViewStripElement)(this.mainPageView.GetChildAt(0))).ItemFitMode = ((Telerik.WinControls.UI.StripViewItemFitMode)((Telerik.WinControls.UI.StripViewItemFitMode.Shrink | Telerik.WinControls.UI.StripViewItemFitMode.Fill)));
+            ((Telerik.WinControls.UI.RadPageViewStripElement)(this.mainPageView.GetChildAt(0))).ShowItemCloseButton = true;
             // 
             // MainPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1120, 698);
+            this.Controls.Add(this.mainPageView);
+            this.Controls.Add(this.radMenu1);
             this.Name = "MainPage";
             this.Text = "MainPage";
+            ((System.ComponentModel.ISupportInitialize)(this.radMenu1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPageView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Telerik.WinControls.UI.RadMenu radMenu1;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem1;
+        private Telerik.WinControls.UI.RadMenuItem menuBimeAll;
+        private Telerik.WinControls.UI.RadMenuItem menuBimeSales;
+        private Telerik.WinControls.UI.RadMenuItem menuBimeBadane;
+        private Telerik.WinControls.UI.RadMenuItem menuBimeOthers;
+        private Telerik.WinControls.UI.RadMenuItem menuPersons;
+        private Telerik.WinControls.UI.RadMenuItem menuInfo;
+        private Telerik.WinControls.UI.RadMenuItem menuDatacenter;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem9;
+        private Telerik.WinControls.UI.RadMenuItem radMenuItem10;
+        private Telerik.WinControls.Themes.VisualStudio2012LightTheme visualStudio2012LightTheme1;
+        private Telerik.WinControls.UI.RadPageView mainPageView;
     }
 }
--- BimeCRM/BimeCRM/MainPage.cs
+++ BimeCRM/BimeCRM/MainPage.cs
@@ -7,14 +7,119 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Telerik.WinControls.UI;
 
 namespace BimeCRM
 {
     public partial class MainPage : Form
     {
+
+        /// Parameters
+        private List<int> mainPageOpenPages = new List<int>();
+
+
         public MainPage()
         {
             InitializeComponent();
         }
+
+        private void menuDatacenter_Click(object sender, EventArgs e)
+        {
+            AddPageInMainPageView(0); 
+        }
+
+
+        private void menuBimeAll_Click(object sender, EventArgs e)
+        {
+            AddPageInMainPageView(1);
+        }
+
+        private void menuBimeSales_Click(object sender, EventArgs e)
+        {
+            AddPageInMainPageView(2);
+        }
+
+        private void menuBimeBadane_Click(object sender, EventArgs e)
+        {
+            AddPageInMainPageView(3);
+        }
+
+        private void menuBimeOthers_Click(object sender, EventArgs e)
+        {
+            AddPageInMainPageView(4);
+        }
+
+        private void menuPersons_Click(object sender, EventArgs e)
+        {
+            AddPageInMainPageView(5);
+        }
+
+
+        private void AddPageInMainPageView(int pageType)
+        {
+            //ToDo: complete it
+            
+            bool existPage = false;
+
+            for (int i = 0; i < mainPageOpenPages.Count; i++)
+            {
+                if (mainPageOpenPages[i] == pageType)
+                {
+                    existPage = true;
+                }
+            }
+
+            if (!existPage)
+            {
+                RadPageViewPage pageViewPage;
+                switch (pageType)
+                {
+                    case 0:
+                        pageViewPage = new RadPageViewPage("مدیریت داده ها");
+                        
+                        break;
+                    case 1:
+                        pageViewPage = new RadPageViewPage("همه بیمه ها");
+                        break;
+                    case 2:
+                        pageViewPage = new RadPageViewPage("بیمه های ثالث");
+                        break;
+                    case 3:
+                        pageViewPage = new RadPageViewPage("بیمه های بدنه");
+                        break;
+                    case 4:
+                        pageViewPage = new RadPageViewPage("سایر بیمه ها");
+                        break;
+                    case 5:
+                        pageViewPage = new RadPageViewPage("بیمه گذاران");
+                        break;
+
+                    default:
+                        pageViewPage = new RadPageViewPage("");
+                        break;
+                }
+
+                pageViewPage.Name = pageType.ToString(); //set name to find it for focus
+                mainPageView.Pages.Add(pageViewPage); // add page in view
+                mainPageOpenPages.Add(pageType); // add pageType in list of open page
+                mainPageView.SelectedPage = mainPageView.Pages[pageType.ToString()]; // focus on page that created
+            }
+            else
+            {
+                // todo : if exist page show that
+
+
+                mainPageView.SelectedPage = mainPageView.Pages[pageType.ToString()];
+
+            }
+
+        }
+
+        private void mainPageView_PageRemoved(object sender, RadPageViewEventArgs e)
+        {
+            mainPageOpenPages.Remove(mainPageOpenPages.IndexOf(Int32.Parse(e.Page.Name)));
+        }
+
+       
     }
 }
